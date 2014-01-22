2db177f81e744b5e65162c10174c778092761c38
Nejuf <nejuf1@gmail.com>
2014-01-21 17:28:42 -0800

    Adds Highscores controller

---
+++ app/controllers/highscores_controller.rb
@@ -0,0 +1,25 @@
+class HighscoresController < ApplicationController
+  def index
+  	@highscores = Highscore.all
+  	respond_to do |f|
+  		f.html
+  		f.xml { render xml: @highscores }
+  		f.json { render json: @highscores }
+  	end
+  end
+
+  def create
+  	@highscore = Highscore.new(highscore_params)
+
+  	if @highscore.save
+  		render json: @highscore
+  	else
+  		render json: { errors: @highscore.errors.full_messages }, status: 422
+  	end
+  end
+
+  private
+  def highscore_params
+  	params.permit(:name, :score, :ip)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsScoreServer::Application.routes.draw do
+  root 'highscores#index'
+  resources :highscores, only: [:index, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
