453e4b7f6d820e6fa4753f82cd60652e49e654ec
Nejuf <nejuf1@gmail.com>
2014-01-21 16:11:53 -0800

    Sets up Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsScoreServer::Application.config.secret_key_base = '379009db7cef68c16a645d12733d24f3dbf5e5ae2421f025af8c9d6e2e32deaae69f3da2057cd6f981593e79e30712c498a29452214da3494d3d506a597d6196'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsScoreServer::Application.config.session_store :cookie_store, key: '_RailsScoreServer_session'
